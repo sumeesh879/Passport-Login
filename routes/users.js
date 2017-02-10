ccb7c387c97e1ce9e550cfe3e26e6be9b2c0e6fc
sumeesh <sumeesh.nagisetty@gmail.com>
2017-02-10 12:18:10 -0500

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+// Register
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+
+// Register
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+module.exports = router;
